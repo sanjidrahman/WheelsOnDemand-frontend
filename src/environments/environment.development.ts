d7afd4efcb0c6599962d33389ee97600a5b0535d
Sanjid Rahman <rahmansanjid505@gmail.com>
2023-10-27 12:16:43 +0530

    Untrack src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    API_URL: 'http://localhost:3000',
-    CLIENT_ID: '944511069717-viocd97ecmdd9k559qg24hvnv2n3pog5.apps.googleusercontent.com',
-    STATIC_FILE_API: 'http://localhost:3000/admin/getFile/'
-};
