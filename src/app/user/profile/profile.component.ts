aa063f4371b102ef60f2bdd7d88235c8b3de721d
Sanjid Rahman <rahmansanjid505@gmail.com>
2023-11-06 09:00:09 +0530

    some bug fixes

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -7,6 +7,8 @@ import { getuser } from "src/app/store/state/app.selectors";
 import { Observable, map } from "rxjs";
 import { userModel } from "src/app/models/user.model";
 import { environment } from "src/environments/environment.development";
+import { UserService } from "../services/user.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
@@ -18,12 +20,31 @@ export class ProfileComponent implements OnInit {
   isHovered: boolean = false;
   userid: any
   userDetails!: Observable<userModel | undefined>
+  profile!: File | null
+  userForm!: FormGroup
+  changePassForm!: FormGroup
+  isEditable: boolean = false;
 
   constructor(
     private _store: Store<userState>,
+    private _service: UserService,
+    private _fb: FormBuilder
   ){}
 
   ngOnInit(): void {
+
+    this.userForm = this._fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required]]
+    })
+
+    this.changePassForm = this._fb.group({ 
+      oldpass: ['' , [Validators.required]],
+      newpass: ['' , [Validators.required]],
+      confirmpass: ['', [Validators.required]]
+    })
+
     const token = localStorage.getItem('userToken');
     if (token) {
       this.userid = jwt_decode(token)
@@ -35,13 +56,25 @@ export class ProfileComponent implements OnInit {
     )
   }
 
-  onFileSelected(event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
-    const selectedFile = inputElement.files?.[0];
-    if (selectedFile) {
-      // Handle the selected file (e.g., upload it to the server)
-      // You can add your logic to update the user's profile image.
+  onProfileSelected(event: any): void {
+    const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    this.profile = <File>event.target.files[0];
+
+    if (this.profile) {
+      const fileExtension = this.getFileExtension(this.profile.name);
+
+      if (!allowedExtensions.includes(fileExtension)) {
+        this.profile = null;
+        alert('Please select a valid image file (jpg, jpeg, png, or gif).');
+      }
     }
+  }
+  getFileExtension(filename: string): any {
+    return filename.split('.').pop();
+  }
+
+  edit() {
+    this.isEditable = true
   }
 
   getImage(file: string) {
